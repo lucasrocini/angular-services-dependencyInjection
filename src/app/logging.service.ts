e2d566318702567b83ca8e1cdc5cb16f6f3eee60
Lucas <lucasrocini@hotmail.com>
2021-09-04 16:37:50 -0300

    End of Learning Module

--- src/app/logging.service.ts
+++ src/app/logging.service.ts
@@ -1,4 +1,10 @@
-export class LoggingService {
+import { Injectable } from "@angular/core";
+
+@Injectable({
+    providedIn: 'root'
+  })
+export class 
+LoggingService {
     logStatusChange(status: string) {
         console.log('A server status changed, new status: ' + status);
     }
--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -1,23 +1,31 @@
 import { serializeNodes } from '@angular/compiler/src/i18n/digest';
 import { Component, EventEmitter, Output } from '@angular/core';
+import { AccountsService } from '../account.service';
 import { LoggingService } from '../logging.service';
 
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
   styleUrls: ['./new-account.component.css'],
-  providers: [LoggingService]
+  // providers: [LoggingService] //AccountsService
 })
 export class NewAccountComponent {
   @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
 
-constructor(private loggingService: LoggingService) { }
+constructor(private loggingService: LoggingService,
+            private accountsService: AccountsService) {
+  this.accountsService.statusUpdated.subscribe(
+    (status: string) => alert('New Status: ' + status)
+  );
+}
 
   onCreateAccount(accountName: string, accountStatus: string) {
-    this.accountAdded.emit({
-      name: accountName,
-      status: accountStatus
-    });
+    // this.accountAdded.emit({
+    //   name: accountName,
+    //   status: accountStatus
+    // });
+    this.accountsService.addAccount(accountName, accountStatus);
+    // this.loggingService.logStatusChange(accountStatus);
 
     // Example of what not to do to call an angular service
     // const service = new LoggingService();
